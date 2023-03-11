30d46501ffb5d7e4f1ce0b167ea15e404668334c
yoophglup <yoophglup@gmail.com>
2023-03-10 18:46:53 -0600

    Initial Commit

---
+++ src/main/java/com/SpringCP/SpringCP/SpringCpApplication.java
@@ -0,0 +1,14 @@
+package com.SpringCP.SpringCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringCP/SpringCP/SpringCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringCP.SpringCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
